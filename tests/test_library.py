0aa99a43432975e065e79f3a13ef84fa524f978a
Aryan <singhvishesharyan@gmail.com>
2026-01-21 23:32:54 +0530

    Sprint 2: Borrow and return books

--- tests/test_library.py
+++ tests/test_library.py
@@ -13,3 +13,26 @@ class TestLibrarySprint1(unittest.TestCase):
         lib.add_book("B1", "Python", "Guido")
         with self.assertRaises(ValueError):
             lib.add_book("B1", "Java", "James")
+            
+class TestLibrarySprint2(unittest.TestCase):
+
+    def test_borrow_book(self):
+        lib = Library()
+        lib.add_book("B1", "Python", "Guido")
+        lib.borrow_book("B1")
+        self.assertTrue(lib.books["B1"]["borrowed"])
+
+    def test_borrow_unavailable_book(self):
+        lib = Library()
+        lib.add_book("B1", "Python", "Guido")
+        lib.borrow_book("B1")
+        with self.assertRaises(ValueError):
+            lib.borrow_book("B1")
+
+    def test_return_book(self):
+        lib = Library()
+        lib.add_book("B1", "Python", "Guido")
+        lib.borrow_book("B1")
+        lib.return_book("B1")
+        self.assertFalse(lib.books["B1"]["borrowed"])
+
